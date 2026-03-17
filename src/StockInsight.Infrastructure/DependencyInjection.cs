4244f3ea265f36656e76d4abb3bc6fccfa1e5caf
enqer <pawel.kam4@gmail.com>
2026-03-17 22:54:06 +0100

    chore: dj

--- src/StockInsight.Infrastructure/DependencyInjection.cs
+++ src/StockInsight.Infrastructure/DependencyInjection.cs
@@ -7,7 +7,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
     {
-
         return services;
     }
 }
